4676eaca4c8a5142a0b1c675cf1a41decd544fbc
wangak kim <kimgak22@gmail.com>
2021-03-28 23:50:12 +0900

    Rename PaintData -> Paint

--- FreeWheelinTest/Sources/Models/Paint.swift
+++ FreeWheelinTest/Sources/Models/Paint.swift
@@ -1,13 +1,13 @@
 //
-//  PaintData.swift
+//  Paint.swift
 //  FreeWheelinTest
 //
 //  Created by Wangak Kim on 2021/03/27.
 //  Copyright © 2021 Wangak Kim. All rights reserved.
 //
 	
-import Foundation
+import UIKit
 
-struct PaintData {
-    
+struct Paint: Codable {
+    var imageData: Data?
 }
